7ec00b749388ded0703beb927e974f48de6d0ab0
gorety <wagutugorety@gmail.com>
2023-04-19 11:32:02 +0300

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-import React from "react";
-import ColorList from "./ColorList";
+import React from "react"
 
-function App() {
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+  function ColorItem(props) {
+    return <li style={{ color: props.color }}>{props.color}</li>;
+  }
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+
   return (
     <div>
-      <ColorList />
+      <h1>Top 5 CSS Beautiful Colors</h1>
+      <ol>
+        <li style={{ color: colors[0] }}>{colors[0]}</li>
+        <li style={{ color: colors[1] }}>{colors[1]}</li>
+        <li style={{ color: colors[2] }}>{colors[2]}</li>
+        <li style={{ color: colors[3] }}>{colors[3]}</li>
+        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
+      </ol>
+    </div>
+  );
+}
+
+function App() {
+  
+  return (
+    <div>
+      <ColorList/>
     </div>
   );
 }
